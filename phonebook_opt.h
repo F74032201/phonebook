990d8b66d8fe579da1709560c7a43b65e4c9c610
wenwei <qscefb5566@gmail.com>
2018-03-11 20:23:29 +0800

    Transform data structure to decrease miss rate
    The size of entry is 136 bytes originally,
    however most of them are not used when appending or finding.
    As a resault, I keep the content except lastName in another struct,
    and set a pointer to it.So that, the cache miss will decrease.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -7,7 +7,7 @@
  * line to set OPT properly */
 #define OPT 1
 
-typedef struct __PHONE_BOOK_ENTRY {
+typedef struct __PHONE_BOOK_OTHER_ENTRY {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,23 +17,19 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} other_entry;
+
+typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
+    other_entry *others;
 } entry;
 
 typedef struct __BST_ENTRY {
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
     char lastName[MAX_LAST_NAME_SIZE];
     struct __BST_ENTRY *left;
     struct __BST_ENTRY *right;
+    other_entry *others;
 } bst_entry;
 
 bst_entry *findName(char lastName[], bst_entry *pHead);
